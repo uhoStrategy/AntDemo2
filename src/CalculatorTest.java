689b1c9b332de54421fbc2cf088b55d14499a901
uho.Strategy <uho@impaqstrategy.com>
2017-03-05 13:14:21 -0500

    Committing

---
+++ src/CalculatorTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void evaluatesExpression() {
+	Calculator calculator = new Calculator();
+	int sum = calculator.evaluate("1+2+3");
+	assertEquals(6, sum);
+		  
+	}
+
+	
+}
